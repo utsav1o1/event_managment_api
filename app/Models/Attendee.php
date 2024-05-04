d3b8eccd5727b362e754540d7eadf281a51911f6
Utsav1o1 <utsavkarki244@gmail.com>
2024-05-04 18:35:31 +0545

    added resource for the api in order to limit the access of the data

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Attendee extends Model
 {
     use HasFactory;
-
+    protected $fillable = ['user_id'];
     public function user() : BelongsTo
     {
         return $this->belongsTo(User::class);
--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,4 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class)
-        ->scoped(['attendee'=>'event']);
+    ->scoped()->except(['update']);
